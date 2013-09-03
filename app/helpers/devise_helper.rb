df67fc6777e4965ce52058fc27a507905c994fc1
Matthew Rennie <matthewrennie@gmail.com>
2013-09-03 08:31:22 -0700

    refactored devise flash messages

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,55 @@
+module DeviseHelper
+
+  def devise_error_messages!
+  	# Method code adapted from: # Gottfrois, P (2013) Devise Error Messages Twitter Bootstrap Style [Source code]. # https://coderwall.com/gottfrois
+  	return '' if resource.errors.empty?  	
+  	
+  	messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+      count: resource.errors.count,
+      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="alert alert-danger">     	
+      <h4><span class="glyphicon glyphicon-ban-circle"></span>&nbsp;#{sentence}</h4>
+      <ul>
+      	#{messages}
+    	</ul>
+    </div>
+    HTML
+
+    html.html_safe
+
+  end
+
+  def devise_flash_messages!
+  	return '' if flash.empty?
+
+  	if(!flash[:alert].nil?)
+
+  		html = <<-HTML
+	    <div class="alert alert-danger">
+				<span class="glyphicon glyphicon-ban-circle"></span>
+				#{flash[:alert]}
+			</div>
+	    HTML
+
+    	return html.html_safe
+
+  	elsif(!flash[:notice].nil?)
+
+  		html = <<-HTML
+	    <div class="alert alert-warning">
+				<span class="glyphicon glyphicon-warning-sign"></span>
+				#{flash[:alert]}
+			</div>
+	    HTML
+
+    	return html.html_safe
+
+  	end
+
+  end
+
+
+end
